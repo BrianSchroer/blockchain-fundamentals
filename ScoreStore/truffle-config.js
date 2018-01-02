e69343f078399ac7b58e094cadb6985adc1bc170
Brian Schroer <brianschroer@gmail.com>
2018-01-02 11:03:27 -0600

    Add production configuration

--- ScoreStore/truffle-config.js
+++ ScoreStore/truffle-config.js
@@ -4,6 +4,12 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*'
+    },
+    production: {
+      host: 'rlcgj3xdz2l7.centralus.cloudapp.azure.com',
+      port: 8545,
+      network_id: '*',
+      gas: 500000
     }
   }
 };
